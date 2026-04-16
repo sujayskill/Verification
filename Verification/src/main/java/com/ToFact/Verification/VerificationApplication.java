b8adc89a4fd93961d53bb1f13677bf7ff9a07b6b
sujay <sujaynarala@gmail.com>
2026-04-16 15:38:11 +0530

    first commit

--- Verification/src/main/java/com/ToFact/Verification/VerificationApplication.java
+++ Verification/src/main/java/com/ToFact/Verification/VerificationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VerificationApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VerificationApplication.class, args);
 	}
