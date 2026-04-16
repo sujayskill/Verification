8c6830033ead48e099c054a73ad5d56c7a6f4116
sujay <sujaynarala@gmail.com>
2026-04-16 22:19:22 +0530

    initial commit

--- src/modules/org/pages/sections/AddCandidate.jsx
+++ src/modules/org/pages/sections/AddCandidate.jsx
@@ -368,7 +368,6 @@ export default function AddCandidate() {
               type="date"
               onChange={(e) => handleChange("End date", e.target.value)}
             />
-
             <label>Payslips</label>
             <input
               type="file"
